ecc930cbc2b9c816f483ad8dda511c4576edcacc
creative301 <rza9001@gmail.com>
2019-02-22 17:07:38 +0700

    Add project 3 from the OpenClassrooms assignment

---
+++ proj3/js/script.js
@@ -0,0 +1,16 @@
+// Intersection Observer API
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if(entry.isIntersecting === true) {
+      entry.target.classList.add('active');
+    } else {
+      entry.target.classList.remove('active');
+    }
+  });
+});
+
+const items = document.querySelectorAll('.featured .row');
+
+items.forEach((item, index) => {
+  observer.observe(item, index);
+});
